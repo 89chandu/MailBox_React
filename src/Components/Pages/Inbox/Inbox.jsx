940faed34ec989db59cd2afb5d72042e16941a08
Chandu Bopche <Chandubopche321@gmail.com>
2023-08-21 22:38:23 +0530

    Deleting Mail TDD

--- src/Components/Pages/Inbox/Inbox.jsx
+++ src/Components/Pages/Inbox/Inbox.jsx
@@ -1,77 +1,60 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { globalActions } from '../../Store/globalSlice';
-import { AiOutlineDelete } from 'react-icons/ai';
-import { useNavigate } from 'react-router-dom';
-const Inbox = () => {
-  const dispatch = useDispatch();
-  const receivedMails = useSelector((state) => state.global.receivedMails);
-  const navigate = useNavigate();
-  useEffect(() => {
-    const getAllMails = async () => {
-      try {
-        const token = JSON.parse(localStorage.getItem('token'))
-        let response = await axios.get('http://localhost:3000/email/get', { headers: { "Authorization": token } });
-        let count = 0;
-        response.data.data.forEach((item) => {
-          if (item.opened == false) count++;
-        })
+import Compose from './Compose';
 
-        dispatch(globalActions.updateUnread(count))
-        dispatch(globalActions.InboxFill(response.data.data));
-      } catch (error) {
-        console.log(error);
-        toast.error('couldnt fetch mails')
-      }
-    }
-    getAllMails()
-  }, [])
-  let id;
-  const MailDeleteHandler = async (e) => {
-    try {
-      e.preventDefault();
-      const token = JSON.parse(localStorage.getItem('token'));
-      const response = await axios.patch(`http://localhost:3000/email/isDelete/${id}`, { nothing: "here" }, { headers: { "Authorization": token } });
-      dispatch(globalActions.deleteMail(id));
+// Mock the toast functions to prevent actual notifications
+jest.mock('react-toastify', () => ({
+  toast: {
+    success: jest.fn(),
+    error: jest.fn(),
+  },
+}));
 
-      toast.success(response.data.msg);
-    } catch (error) {
-      console.log(error);
-      toast.error(error.response.data.msg)
-    }
-  }
-  return (
-    receivedMails && receivedMails.length > 0 ? (
-      receivedMails.map((item) => {
-        return <div key={item._id} className={` rounded-2xl md:w-[80%] mx-auto p-2 flex justify-between  border  my-2  cursor-pointer ${item.opened === true ? `bg-white` : `bg-gray-300`}`}>
-          <div className='flex' onClick={(e) => {
-            e.preventDefault();
-            navigate(`/${item._id}`)
-          }}>
-            <div className='mr-4 flex justify-center items-center '>
-              <img className=' min-h-[40px] min-w-[40px] max-h-[40px] max-w-[40px]  md:max-h-[70px] md:max-w-[70px]  object-cover' src="https://api-private.atlassian.com/users/9cea692d0a59c5e100680165cbbeb496/avatar" alt="" />
-            </div>
-            <div className='flex flex-col overflow-hidden'>
-              <span className='text-gray-800 font-bold'>{item.sender}</span>
-              <span className='text-gray-800 font-medium'>{item.subject}</span>
-              <span className='text-gray-800 truncate '>{item.body}</span>
-            </div>
-          </div>
-          <div className='text-4xl text-gray-500 p-2' onClick={(e) => {
-            id = item._id;
-            MailDeleteHandler(e);
-          }}>
-            <AiOutlineDelete />
-          </div>
-        </div>
-      })
-    ) : (<div className='text-6xl text-gray-800 font-bold text-center w-[50%] mx-auto animate-bounce mt-[30%] '>
-      <p>No emails available</p>
-    </div>))
-}
+test('submits email form with valid inputs', async () => {
+  render(<Compose />);
+  
+  // Mock axios if you want to simulate a successful response
+  jest.spyOn(axios, 'post').mockResolvedValueOnce({ data: { msg: 'Email sent successfully' } });
 
-export default Inbox
+  const receiverInput = screen.getByLabelText('To :');
+  const subjectInput = screen.getByLabelText('Sub :');
+  const editor = screen.getByRole('textbox', { name: 'Compose Email :' });
+  const sendButton = screen.getByRole('button', { name: 'Send' });
+
+  fireEvent.change(receiverInput, { target: { value: 'sahilkumar2275@gmail.com' } });
+  fireEvent.change(subjectInput, { target: { value: 'quitting application' } });
+  fireEvent.change(editor, { target: { value: 'Loream Ipsum' } });
+  fireEvent.click(sendButton);
+
+  await waitFor(() => {
+    expect(axios.post).toHaveBeenCalledWith('http://localhost:3000/email/send', expect.any(Object), expect.any(Object));
+    expect(screen.getByText('Email sent successfully')).toBeInTheDocument(); // Assert toast message
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument(); // Assert no alert message
+  });
+});
+
+test('displays alert on email submission error', async () => {
+  render(<Compose />);
+  
+  // Mock axios to simulate an error
+  jest.spyOn(axios, 'post').mockRejectedValueOnce(new Error('Email submission error'));
+
+  const receiverInput = screen.getByLabelText('To :');
+  const subjectInput = screen.getByLabelText('Sub :');
+  const editor = screen.getByRole('textbox', { name: 'Compose Email :' });
+  const sendButton = screen.getByRole('button', { name: 'Send' });
+
+  fireEvent.change(receiverInput, { target: { value: 'sahilkumar2275@gmail.com' } });
+  fireEvent.change(subjectInput, { target: { value: 'quitting application' } });
+  fireEvent.change(editor, { target: { value: 'Loream Ipsum' } });
+  fireEvent.click(sendButton);
+
+  await waitFor(() => {
+    expect(axios.post).toHaveBeenCalledWith('http://localhost:3000/email/send', expect.any(Object), expect.any(Object));
+    expect(screen.queryByText('Email sent successfully')).not.toBeInTheDocument(); // Assert no toast message
+    expect(screen.getByRole('alert')).toBeInTheDocument(); // Assert alert message
+  });
+});
+
+// ... More test cases ...
